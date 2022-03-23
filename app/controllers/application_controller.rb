b8919892344d047df5ff37f798525598bcdfeb28
sanskar <sanskarsoni111>
2022-03-23 11:25:56 +0530

    after_signin_path_fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-	def after_sign_in_path_for(resource)
-    posts_path
-   end
-
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_session_path(resource_or_scope)
+  end
 end
